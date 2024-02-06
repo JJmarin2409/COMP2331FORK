f7150c4f6abbb5c5c2a28dd0df9ad8e1bb685e8b
Jose Marin <jamrin2409@gmail.com>
2024-02-06 16:50:22 +0000

    update

--- Unit3_Integers_And_Ops/integers_exercises.asm
+++ Unit3_Integers_And_Ops/integers_exercises.asm
@@ -1,5 +1,5 @@
 ; Base Author: Megan Avery Spring 2024
-; Exercise Author: [YOUR NAME HERE]
+; Exercise Author: Jose Marin
 ;
 ; Purpose - to learn about the following:
 ;	- sign extension, decreasing/ increasing data size
@@ -23,7 +23,33 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+        ; Making bit smaller
+        mov eax, -34              ; saves -34 into eax
+        dump_regs 1               ; EAX = FFFFFFDE // Dumps regs 
+
+        call print_nl
+
+        mov eax, 0                ; saves 0 into eax
+        mov al, 0DEH              ; saves -34 into eax
+        dump_regs 2               ; EAX = 000000DE // Dumps regs 
+        call print_nl
+
+        call print_int 
+
+        mov eax, 0FFFFFFFFH
+        mov al, 0DEH
+        call print_nl
+
+        call print_int
+        call print_nl
+
+        mov eax, -1
+        mov ax, 58
+        dump_regs 3  
+        call print_nl
+
+        movzx eax, ax
+        dump_regs 4
 
         popa
         mov     eax, 0            ; return back to C
